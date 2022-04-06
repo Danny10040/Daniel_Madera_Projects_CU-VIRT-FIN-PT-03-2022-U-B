b0ba509d097c423bb72815acc5a1a097646e5df4
Danny10040 <92892359+Danny10040@users.noreply.github.com>
2022-04-06 00:31:51 -0400

    Final commit
    Final commit of the first challenge.

--- Challenge 1/loan_analyzer.py
+++ Challenge 1/loan_analyzer.py
@@ -12,9 +12,16 @@ First, let's start with some calculations on a list of prices for 5 loans.
     3. Using the sum of all loans and the total number of loans, calculate the average loan price.
     4. Print all calculations with descriptive messages.
 """
+
 loan_costs = [500, 600, 200, 1000, 450]
+
+#Calculate number of loans
 total_number_of_loans = len(loan_costs)
+
+#Calculate sum of loans
 sum_of_loans = sum(loan_costs)
+
+#calculate average of loans
 average_of_loans = sum_of_loans / total_number_of_loans
 
 # How many loans are in the list?
@@ -65,10 +72,17 @@ loan = {
 
 # @TODO: Use get() on the dictionary of additional information to extract the Future Value and Remaining Months on the loan.
 # Print each variable.
-future_value = loan.get("future_value", 1000)
-remaining_months = loan.get("remaining_months", 9)
+
+#Extract the "future_value" of the "loan" dictionary
+future_value = loan.get("future_value")
+
+#Extract the "remaining_months" of the "loan" dictionary
+remaining_months = loan.get("remaining_months")
+
+#Print both values
 print(f"The future value of the loan is {future_value}.")
 print(f"There are {remaining_months} months remaining.")
+
 # @TODO: Use the formula for Present Value to calculate a "fair value" of the loan.
 # Use a minimum required return of 20% as the discount rate.
 #   You'll want to use the **monthly** version of the present value formula.
@@ -76,6 +90,7 @@ print(f"There are {remaining_months} months remaining.")
 
 minimum_required_return = 0.20 #defines the Discount Rate, or in this case, the minimum required return threshold
 present_value = future_value / (1 + minimum_required_return / 12) ** remaining_months
+print(f"The present value of the loan is ${present_value}.")
 
 # If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
 # @TODO: Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
@@ -110,17 +125,30 @@ new_loan = {
 # @TODO: Define a new function that will be used to calculate present value.
 #    This function should include parameters for `future_value`, `remaining_months`, and the `annual_discount_rate`
 #    The function should return the `present_value` for the loan.
-new_loan.update({"annual_discount_rate": 0.20})
-new_loan_price = new_loan.get("loan_price", 800)
-new_loan_remaining_months = new_loan.get("remaining_months", 12)
-new_loan_repayment_interval = new_loan.get("repayment_interval", "bullet")
-new_loan_future_value = new_loan.get("future_value", 1000)
-new_loan_present_value = new_loan_future_value / (1 + minimum_required_return / 12) ** remaining_months
+
+# Defines a simple present value calculation function where the arguments are the variables in the formula, and then prints the results
+
+def present_value_function_simp(future_value_fn,remaining_months_fn,annual_discount_rate_fn):
+    present_value_fn = future_value_fn / (1 + annual_discount_rate_fn / 12) ** remaining_months_fn
+    print(f"The present value as a result of the simple present value function is {present_value_fn}.")
+
+# Defines a complex present value calculation whose arguments are the discount rate key and discount rate value, where the 
+# function then adds the arguments to the "new_loan" dictionary (for archival purposes), and then extracts them to perform the calculation, and then
+# prints the results
+
+def present_value_function_compl(discount_rate_key,annual_discount_rate_fn_compl):
+    new_loan[discount_rate_key] = annual_discount_rate_fn_compl
+    present_value_fn_compl = new_loan.get("future_value") / (1 + new_loan.get(discount_rate_key)/ 12) ** new_loan.get("remaining_months")
+    print(f"The present value as a result of the complex present value function is {present_value_fn_compl}.")
+
 # @TODO: Use the function to calculate the present value of the new loan given below.
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
-new_loan_discount_rate = 0.2
-new_loan_present_value = new_loan_future_value / (1 + new_loan_discount_rate / 12) ** new_loan_remaining_months
-print(f"The present value of the new loan is: {new_loan_present_value}")
+
+#Calls both functions defined above
+
+present_value_function_simp(1000,12,0.2)
+
+present_value_function_compl("discount_rate",0.2)
 
 
 """Part 4: Conditionally filter lists of loans.
@@ -169,22 +197,46 @@ print(f"At this point, the inexpensive_loans list is: {inexpensive_loans}.")
 
 # @TODO: Loop through all the loans and append any that cost $500 or less to the `inexpensive_loans` list
 
-# extracts all loan prices from list of dictionaries and prints them
-all_loan_prices = [all_loans["loan_price"] for all_loans in list_of_loans]
-print(f"All the loan amounts are ${all_loan_prices}.")
 
-# extracts all loan prices $500 and under and adds them to the inexpensive_loans list, and does the same for those over $500 and adds them to the expensive_loans list
-for cheap_loans in all_loan_prices:
-    if cheap_loans <=500:
-        inexpensive_loans.append(cheap_loans)
-expensive_loans = []
-for pricey_loans in all_loan_prices:
-    if pricey_loans > 500:
-        expensive_loans.append(pricey_loans)
+# Separate the list of dictionaries into 4 separate dictionaries
+
+l1, l2, l3, l4 = [(l5) for l5 in list_of_loans]
+
+# check the four dictionaries for loan price of $500 and under
+
+for cheap_loans1 in l1:
+    l1_cont_price = l1["loan_price"]
+    if l1_cont_price <= 500:
+        l1_cont_str = (l1.values())
+        inexpensive_loans.append(list(l1_cont_str))
+        break
+
+for cheap_loans1 in l1:
+    l1_cont_price = l2["loan_price"]
+    if l1_cont_price <= 500:
+        l1_cont_str = (l2.values())
+        inexpensive_loans.append(list(l1_cont_str))
+        break
+
+for cheap_loans1 in l1:
+    l1_cont_price = l3["loan_price"]
+    if l1_cont_price <= 500:
+        l1_cont_str = (l3.values())
+        inexpensive_loans.append(list(l1_cont_str))
+        break
+
+for cheap_loans1 in l1:
+    l1_cont_price = l4["loan_price"]
+    if l1_cont_price <= 500:
+        l1_cont_str = (l4.values())
+        inexpensive_loans.append(list(l1_cont_str))
+        break
+
+print(f"The inexpensive loans are {inexpensive_loans}.")
+
 
 # @TODO: Print the `inexpensive_loans` list, and the expensive_loans list
-print(f"the inexpensive loans cost ${inexpensive_loans}.")
-print(f"the expensive loans cost ${expensive_loans}.")
+
 
 """Part 5: Save the results.
 
@@ -200,29 +252,34 @@ Output this list of inexpensive loans to a csv file
 
 """
 
-#extract data from dictionaries and place them into separate lists
 
-all_remaining_months = [all_months["remaining_months"]for all_months in list_of_loans]
-all_repayment_intervals = [all_intervals["repayment_interval"]for all_intervals in list_of_loans]
-all_future_values = [all_futures["future_value"]for all_futures in list_of_loans]
+# Separate the values from inexpensive_loans for rows
 
-
-#list_loan_price = [all_loan_prices]
-#list_remaining_months = [all_remaining_months]
-
+l6, l7 = [(l8) for l8 in inexpensive_loans]
 
 # Set the output header
 header = ["loan_price", "remaining_months", "repayment_interval", "future_value"]
+# creates a list of a list so that the header and data doesn't open a csv with letters separated by commas
+headers = [header]
+rows2 = [l6]
+rows3 = [l7]
 
-print(header)
-print(all_loan_prices)
-print(all_remaining_months)
-print(all_repayment_intervals)
-print(all_future_values)
 
 # Set the output file path
 output_path = Path("inexpensive_loans.csv")
 
 # @TODO: Use the csv library and `csv.writer` to write the header row
 # and each row of `loan.values()` from the `inexpensive_loans` list.
-# YOUR CODE HERE!
+
+
+# Contains the arguemnt to open the csv file in w mode
+file = open(output_path, 'w+',newline='')
+# Opens csv file
+with file:
+    write = csv.writer(file)
+    # Write the rows for header
+    write.writerows(headers)
+    write.writerows(rows2)
+    write.writerows(rows3)
+
+print("This module was completed on Apr 06, 2022 at 12:11 PM")
